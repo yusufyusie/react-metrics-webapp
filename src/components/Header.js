da0b4b504cad4bb4201001750d2f43d594b13cc4
yusufyusie <yesuf023@gmail.com>
2023-09-20 00:14:45 +0300

    Add headers styles to webapp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import back from '../assets/arrow_back.svg';
+import menu from '../assets/menu.png';
+import '../styles/Header.css';
 
 const Header = () => (
   <header>
     <div className="left">
       <Link to="/">
+        <img src={back} alt="back" />
       </Link>
       <h1>Game of Thrones Character</h1>
     </div>
+    <img src={menu} alt="menu" />
   </header>
 );
 
