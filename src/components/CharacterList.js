a5b65ce46bd9a636ddd151b63ad73b1c1cf4b357
yusufyusie <yesuf023@gmail.com>
2023-09-20 00:45:13 +0300

    Add search filed

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,21 @@
+import React, { useState } from 'react';
 import banner from '../assets/banner.png';
+import searchIcon from '../assets/find.svg';
 
 const CharacterList= () => {
+    const [search, setSearch] = useState('');
 
     return (
         <section className="character-container">
             <img src={banner} alt="banner" />
             <article className="search-field">
-                
+            <img src={searchIcon} alt="search icon" />
+            <input
+            type="search"
+            placeholder="Search Characters"
+            onChange={''}
+            value={search}
+        />
             </article>
 
         </section>
