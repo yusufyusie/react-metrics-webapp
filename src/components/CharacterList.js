4dd82f05eb741a9a97a81dd929fadbeada9c73f0
yusufyusie <yesuf023@gmail.com>
2023-09-20 00:38:38 +0300

    Add baner to webapp project

---
+++ src/components/CharacterList.js
@@ -0,0 +1,16 @@
+import banner from '../assets/banner.png';
+
+const CharacterList= () => {
+
+    return (
+        <section className="character-container">
+            <img src={banner} alt="banner" />
+            <article className="search-field">
+                
+            </article>
+
+        </section>
+     );
+}
+
+export default CharacterList;
