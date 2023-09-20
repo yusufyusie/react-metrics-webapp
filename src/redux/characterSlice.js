b2999664d108492c5380fc28c6759fe2b296e26f
yusufyusie <yesuf023@gmail.com>
2023-09-20 20:37:33 +0300

    Add getAllCharacters that use createAsyncThunk

--- src/redux/characterSlice.js
+++ src/redux/characterSlice.js
@@ -1,11 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+const getCharactersUrl = 'https://thronesapi.com/api/v2/Characters';
 
 const initialState = { 
     charactersData: [], 
     loading: false, 
-    error: '' 
+    error: '',
    };
+
+   export const getAllCharacters = createAsyncThunk('characters/fetchCharacters', async () => {
+          const response = await fetch(getCharactersUrl,
+            {
+                method: 'GET',
+                headers: {
+                    'content-Type': 'application/json; charset=utf-8',
+                },
+            });
+            const result = await response.json();
+             return result.characters;
+   });
 
 const characterSlice = createSlice({
     name: 'character',
