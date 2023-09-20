e12c05d2a1ef2ca242cf43dbf508ee0ee6ffdbde
yusufyusie <yesuf023@gmail.com>
2023-09-20 07:35:07 +0300

    Add intialstate

---
+++ src/redux/characterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = { 
+    loading: false, 
+    charactersData: [], 
+    error: '' 
+   };
+
+const characterSlice = createSlice({
+
+})
+
+export default characterSlice.reducer;
