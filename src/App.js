1947407af40fd112649258ceb73d51e6cc72ee11
yusufyusie <yesuf023@gmail.com>
2023-09-20 07:03:36 +0300

    Add footer to webapp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import CharacterList from './components/CharacterList';
+import Footer from './components/Footer'
 
 const App = () => (
   <Router>
     <Header />
     <Routes>
     <Route path="/" element={<CharacterList />} />
+    <Footer />
     </Routes>
   </Router>
 );
