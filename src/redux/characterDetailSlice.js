7a2b8a223dc3b1da61dbca6462e1ea43e34866c0
yusufyusie <yesuf023@gmail.com>
2023-09-21 14:42:14 +0300

    Add reducers and extrareducers

--- src/redux/characterDetailSlice.js
+++ src/redux/characterDetailSlice.js
@@ -2,8 +2,33 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { getCharactersUrl } from './characterSlice'
 import axios from "axios";
 
-export const getCoinDetails = createAsyncThunk('details/characterDetails', async (id) => {
+export const getCharacterDetails = createAsyncThunk('details/characterDetails', async (id) => {
     const response = await axios.get(getCharactersUrl/{id});
     const { actor } = await response.json();
     return actor;
   });
+
+  const initialState = { loading: false, characterDetails: [], error: '' };  
+
+  const characterDetailSlice = createSlice({
+    name: 'details',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder.addCase(getCharacterDetails.pending, (state) => {
+        const newState = { ...state, loading: true };
+        return newState;
+      });
+      builder.addCase(getCharacterDetails.fulfilled, (state, action) => {
+        const newState = { ...state, characterDetails: action.payload, loading: false };
+        return newState;
+      });
+      builder.addCase(getCharacterDetails.rejected, (state, action) => {
+        const newState = { ...state, characterDetails: [], error: action.error.message };
+        return newState;
+      });
+    },
+  });
+  
+  export default characterDetailSlice.reducer;
+  
