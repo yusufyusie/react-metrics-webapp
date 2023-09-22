91d7992218d7e9e293a319b67db22ee043a5abf3
yusufyusie <yesuf023@gmail.com>
2023-09-22 21:52:41 +0300

    Add details for each actor

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -8,18 +8,62 @@ const CharacterDetails = () => {;
     const params = useParams();
     const id = params.id || 0;
     const {characters}= useSelector((state) => state.character);
-     const character = characters.find(item =>item.id===id)
+     const character = characters.find(item =>item.id==id)
     console.log(character);
-    console.log(params);
     
   
     return (
-      <div className="actor-details">
-        <div className="hero">
-          <img src={''} alt="actor-icon" />
-          <h2>fullName</h2>
-        </div>
-      </div>
+      <section className="character-container">
+  <article className="character-list">
+          <div className="actorCard">
+            <div className="actorImage">
+              {(character.imageUrl)
+                  ? <img 
+                        width={228}
+                        height={343}
+                        alt="Actor" src={character.imageUrl} />
+                        : <p>no image available</p>}
+                      <div className="title">
+                          {character.fullName}
+                          &apos;s details
+                      </div>
+            </div>
+            <div className="actorDesc">
+            <ul className="details-info">
+                <li className="details-item">
+                  <span className="details-name">First Name</span>
+                  <span className="details-response">{character.firstName}</span>
+                </li>
+                <li className="details-item">
+                  <span className="details-name">Last Name:</span>
+                  <span className="details-response">{character.lastName}</span>
+                </li>
+                <li className="details-item">
+                  <span className="details-name">Full Name: </span>
+                  <span className="details-response">{character.fullName}</span>
+                </li>
+                <li className="details-item">
+                  <span className="details-name">Title:</span>
+                  <span className="details-response">{character.title}</span>
+                </li>
+                <li className="details-item">
+                  <span className="details-name">Family:</span>
+                  <span className="details-response">{character.family}</span>
+                </li>
+                <li className="details-item">
+                  <span className="details-name">Image:</span>
+                  <span className="details-response">{character.image}</span>
+                </li>
+                <li className="details-item">
+                  <span className="details-name">Image URL:</span>
+                  <span className="details-response">{character.imageUrl}</span>
+                </li>
+
+              </ul>
+            </div>
+         </div>
+      </article>           
+  </section>
     );
   };
   
