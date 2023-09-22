ad51f3029c553463e1898b402e28af27937637cf
yusufyusie <yesuf023@gmail.com>
2023-09-23 01:13:28 +0300

    Add characterlist testing

--- src/__test__/configureStore.test.js
+++ src/__test__/configureStore.test.js
@@ -2,12 +2,7 @@ import { getAllCharacters } from '../redux/characterSlice';
 import store from '../redux/configureStore'
 
 
-describe('configureStore', () => {
-    test('character that the state is empty', () => {
-      const initialState = store.getState().character;
-      expect(initialState).toEqual({ characters: [], Loading: false, error: null });
-    });
-  
+describe('configureStore', () => {  
     test('characters that the state is having the data', async () => {
       await store.dispatch(getAllCharacters());
       const updatedState = store.getState().character;
